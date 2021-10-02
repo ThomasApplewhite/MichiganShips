e9b20ceb77965eca4211b4c741571149b4fdbac1
ThomasApplewhite <44822750+ThomasApplewhite@users.noreply.github.com>
2021-10-01 19:03:09 -0700

    Higher Quality Hit Detection and Damage
    Cannonballs can now damage individual parts of ships.
    
    Because CharacterController is a 3D collider, each part of the ship needs 3D colliders as well. This isn't ideal, but it's just way too convenient to not use. I should make a 2D version one of these days...

---
+++ MichiganShips/MichiganShipss/Assets/Scripts/ShipHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipHealth : MonoBehaviour
+{
+    public int health = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public virtual void TakeDamage(int damage)
+    {
+        health -= damage;
+        if(health <= 0) Debug.Log($"{this.gameObject.name}.ShipHealth.TakeDamage: Dead!");
+    }
+}
